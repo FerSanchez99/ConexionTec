0d895f49f45b5ee9bf8dd722ba146436884e5565
abqu <erick_abqu@hotmail.com>
2021-10-23 10:38:06 -0500

    Update social_impact.rb

--- app/models/social_impact.rb
+++ app/models/social_impact.rb
@@ -1,3 +1,16 @@
 class SocialImpact < ApplicationRecord
+<<<<<<< Updated upstream
+=======
+
+	validates :problem, presence: true, uniqueness: false, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain social problem"}
+
+    validates :empathy, presence: true, uniqueness: false, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain problem"}
+
+    validates :responsibility, presence: true, uniqueness: false, format: {with: /[
+    a-zA-Z0-9_-]{2}/,message: "must contain problem"}
+
+>>>>>>> Stashed changes
   belongs_to :project
 end
