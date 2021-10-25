a64d6a60eb984c2bc68d7db41cac408ede5ac765
abqu <erick_abqu@hotmail.com>
2021-10-24 23:31:19 -0500

    ajustes al mensaje de validacion

--- app/models/social_impact.rb
+++ app/models/social_impact.rb
@@ -1,13 +1,13 @@
 class SocialImpact < ApplicationRecord
 
 	validates :problem, presence: true, uniqueness: false, format: {with: /[
-    a-zA-Z0-9_-]{2}/,message: "must contain social problem"}
+    a-zA-Z0-9_-]{2}/,message: "must contain text"}
 
     validates :empathy, presence: true, uniqueness: false, format: {with: /[
-    a-zA-Z0-9_-]{2}/,message: "must contain problem"}
+    a-zA-Z0-9_-]{2}/,message: "must contain text"}
 
     validates :responsibility, presence: true, uniqueness: false, format: {with: /[
-    a-zA-Z0-9_-]{2}/,message: "must contain problem"}
+    a-zA-Z0-9_-]{2}/,message: "must contain text"}
 
 
   belongs_to :project
